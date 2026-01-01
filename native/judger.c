d8c0a84b1e3aa7981c1147b1cc0d44d2a349bd6e
naibaf-1 <175958349+naibaf-1@users.noreply.github.com>
2026-01-01 15:12:27 +0100

    Correct usage of writeFile() depending on the language.

--- native/judger.c
+++ native/judger.c
@@ -6,23 +6,41 @@
 // If called this function receives 3 String and it returns one as soon as it's done
 char* run_judge(const char* usersCode, const char* programmingLanguage, const char* solution) {
     char* fileName;
+    
     // TODO Perform the file generation & compilation depending on the used language
-    if (programmingLanguage == ".c")
-    {
-        /* code */
-    } else if (programmingLanguage == ".java")
-    {
-        /* code */
-    } else if (programmingLanguage == ".py")
-    {
-        /* code */
+    if (programmingLanguage == ".c") {
+        // Write the users code into a file with the correct ending of the language he used
+        fileName = writeFile(usersCode, programmingLanguage);
+        if (fileName == "-1"){
+            return "-1";
+        }
+        free(fileName);
+    } else if (programmingLanguage == ".java") {
+        // Write the users code into a file with the correct ending of the language he used
+        fileName = writeFile(usersCode, programmingLanguage);
+        if (fileName == "-1"){
+            return "-1";
+        }
+        free(fileName);
+    } else if (programmingLanguage == ".py") {
+        // Write the users code into a file with the correct ending of the language he used
+        fileName = writeFile(usersCode, programmingLanguage);
+        if (fileName == "-1"){
+            return "-1";
+        }
+        free(fileName);
+    } else if (programmingLanguage == ".cpp") {
+        // Write the users code into a file with the correct ending of the language he used
+        fileName = writeFile(usersCode, programmingLanguage);
+        if (fileName == "-1"){
+            return "-1";
+        }
+        free(fileName);
+    } else {
+        printf("ERROR: Undefined language (Code 1)\n");
+        return "1";
     }
 
-
-
-    
-    // Write the users code into a file with the correct ending of the language he used
-    fileName = writeFile(usersCode, ".py");
     // TODO Call the compiler and pass the file with the users code
     // TODO Filter the compilers output
     //          - Syntax errors: Return message "Syntax errors. Did you select the correct language?"
