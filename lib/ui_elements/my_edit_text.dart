af86f815b15451080033c3c8f8d00a32b7e04461
naibaf-1 <175958349+naibaf-1@users.noreply.github.com>
2026-01-02 16:39:55 +0100

    Removed unnecessary attributes and added controller in order to be able to receive the text e.g. if the user taps onto a  button

--- lib/ui_elements/my_edit_text.dart
+++ lib/ui_elements/my_edit_text.dart
@@ -6,24 +6,20 @@ class MyEditText extends StatefulWidget {
   final bool multiline;
   final bool autocorrect;
   final bool suggestions;
-  final bool autoSelect;
   final bool autofocus;
   final TextInputType textInputType;
   final String hint;
-  final ValueChanged<String>? onInputDone;
-  final String? text;
+  final TextEditingController controller;
 
   const MyEditText({
     super.key,
     required this.hint,
+    required this.controller,
     this.multiline = true,
     this.suggestions = false,
     this.autocorrect = false,
-    this.autoSelect = false,
-    this.autofocus = false,
+    this.autofocus = true,
     this.textInputType = TextInputType.text,
-    this.onInputDone = null,
-    this.text,
   });
 
   @override
@@ -31,42 +27,15 @@ class MyEditText extends StatefulWidget {
 }
 
 class _MyEditTextState extends State<MyEditText> {
-  late TextEditingController _controller;
-  late FocusNode focusNode;
 
   @override
   void initState() {
     super.initState();
-    _controller = TextEditingController(text: widget.text);
+    //widget.controller = TextEditingController(text: widget.text);
     // Cursor at the end
-    _controller.selection = TextSelection.fromPosition(
-      TextPosition(offset: _controller.text.length),
+    widget.controller.selection = TextSelection.fromPosition(
+      TextPosition(offset: widget.controller.text.length),
     );
-
-    focusNode = FocusNode();
-
-    // Call backend tasks if focus lost
-    focusNode.addListener(() {
-      if (!focusNode.hasFocus) {
-        finalizeInput();
-      }
-    });
-  }
-  // Call backend tasks if closed
-  @override
-  void dispose() {
-    finalizeInput();
-    focusNode.dispose();
-    _controller.dispose();
-    super.dispose();
-  }
-
-  // Perform backend tasks
-  void finalizeInput() {
-    final text = _controller.text.trim();
-    if (text.isNotEmpty) {
-      widget.onInputDone?.call(text);
-    }
   }
 
   @override
@@ -74,22 +43,14 @@ class _MyEditTextState extends State<MyEditText> {
     final int? maxLines = widget.multiline ? null : 1;
 
     return TextField(
-      controller: _controller,
-      focusNode: focusNode,
+      controller: widget.controller,
       autocorrect: widget.autocorrect,
       enableSuggestions: widget.suggestions,
-      selectAllOnFocus: widget.autoSelect,
       autofocus: widget.autofocus,
       maxLines: maxLines,
 
       textInputAction:
           widget.multiline ? TextInputAction.newline : TextInputAction.done,
-
-      onSubmitted: (_) => finalizeInput(),
-      onTapOutside: (_) => finalizeInput(),
-      onEditingComplete: () {
-        finalizeInput();
-      },
 
       decoration: InputDecoration(
         hintText: widget.hint,
