f6e94d4193f40e55de6a54eedfff45019358acb5
naibaf-1 <175958349+naibaf-1@users.noreply.github.com>
2026-01-02 15:56:13 +0100

    Define the bindings of the library

---
+++ lib/utils/judger_bindings.dart
@@ -0,0 +1,44 @@
+import 'dart:ffi';
+import 'package:ffi/ffi.dart';
+
+typedef c_judger_func = Pointer<Utf8> Function(
+  Pointer<Utf8>,
+  Pointer<Utf8>,
+  Pointer<Utf8>,
+);
+
+typedef dart_judger_func = Pointer<Utf8> Function(
+  Pointer<Utf8>,
+  Pointer<Utf8>,
+  Pointer<Utf8>,
+);
+
+// Define the bindings for my library
+class JudgerLib {
+  late final DynamicLibrary _lib;
+  late final dart_judger_func runJudge;
+
+  JudgerLib(this._lib) {
+    runJudge = _lib
+        .lookup<NativeFunction<c_judger_func>>('runJudge')
+        .asFunction();
+  }
+
+  // Function "translating" between the library and the flutter code
+  String callJudger(String code, String programmingLanguage, String solution) {
+    final codePointer = code.toNativeUtf8();
+    final languagePointer = programmingLanguage.toNativeUtf8();
+    final solutionPointer = solution.toNativeUtf8();
+
+    final resultPointer = runJudge(codePointer, languagePointer, solutionPointer);
+
+    final result = resultPointer.toDartString();
+
+    // Free memory, because the library doesn't free it
+    malloc.free(codePointer);
+    malloc.free(languagePointer);
+    malloc.free(solutionPointer);
+
+    return result;
+  }
+}
